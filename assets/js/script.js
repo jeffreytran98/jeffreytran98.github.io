b2428f2c29002be2eb8067f5c0a424ac03221975
Jeffrey Tran <jeffrey@Jeffreys-MacBook-Air.local>
2023-05-27 11:05:38 -0700

    Trimmed some words, added some js files for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function sleep(millis) {
+    return new Promise((resolve) => { 
+        setTimeout(resolve, millis);
+    });
+}
+
+
+async function temp() {
+    console.log("Hello world");
+    await sleep(3000);
+    console.log("HEllo world but after 3 seconds")
+}
+
+temp();

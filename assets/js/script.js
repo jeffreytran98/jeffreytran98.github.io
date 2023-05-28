8b0218270d3ac4c112957e0e64d11d155a515401
Jeffrey Tran <jeffrey@Jeffreys-MacBook-Air.local>
2023-05-28 12:32:14 -0700

    Added the typing animation might want to clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,66 @@
-function sleep(millis) {
-    return new Promise((resolve) => { 
-        setTimeout(resolve, millis);
-    });
+const words = ["Writing Code", "Analyzing Data", "Creating Visualizations"]
+
+let i = 0;
+let timer;
+
+function typingEffect() {
+    //split up the word to get each letter. word is a list of individual letters here
+    let word = words[i].split("");
+    // Setting a boolean (True) to a function?
+	var loopTyping = function() {
+		if (word.length > 0) {
+            //Typing the letter
+            document.getElementById('words').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		// How fast each individual letter gets typed (150 = .150 seconds per letter)
+		timer = setTimeout(loopTyping, 150);
+	};
+	// When word is completely deleted, wait .5 seconds
+	setTimeout(loopTyping, 500);
+};
+
+function deletingEffect() {
+    //split up the word to get each letter. word is a list of individual letters here
+    let word = words[i].split("");
+    //while True
+	var loopDeleting = function() {
+		if (word.length > 0) {
+            //delete a letter
+			word.pop();
+			document.getElementById('words').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		// how fast each individual letter gets deleted (100 = .1 seconds per letter)
+		timer = setTimeout(loopDeleting, 100);
+	};
+	// When finished typing, delay 1.25 seconds
+	setTimeout(loopDeleting,1250);
+};
+
+function wordChange(word, new_word) {
+	document.getElementById(word).innerHTML = new_word
 }
 
 
-async function temp() {
-    console.log("Hello world");
-    await sleep(3000);
-    console.log("HEllo world but after 3 seconds")
+function transition(timing, timing_2) {
+	//timing is the number needed to fadeout completley and then change word instantly
+	//timing_2 is the number needed to fadein completely and then begin the LoopDelete and LoopType
+
+
+
+	$('#me').delay((timing)).fadeIn((timing - 1000), function(){})
+	$('#word').fadeIn(timing_2, function(){})
+	setTimeout(typingEffect, timing_2)
 }
 
-temp();
+transition(3000, 4000)
